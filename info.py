c272ac3ba5da88f626a82a57a0c6483f900fbdbd
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-10-19 10:49:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', '')
-CHNL_LNK = environ.get('CHNL_LNK', '')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', '')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+wVUitwQcus82OGVl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+WTY_jAaAtqI4Nzll')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ramSitaam') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/ramSitaam')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
